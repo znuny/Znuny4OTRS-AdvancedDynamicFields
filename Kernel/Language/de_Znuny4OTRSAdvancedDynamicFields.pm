fa84181bab46003a1eb896430a839e5b87d021e1
Denny Bresch <db@znuny.com>
2016-05-26 17:04:05 +0200

    Improved translation.

--- Kernel/Language/de_Znuny4OTRSAdvancedDynamicFields.pm
+++ Kernel/Language/de_Znuny4OTRSAdvancedDynamicFields.pm
@@ -19,10 +19,18 @@ sub Data {
     $Self->{Translation}->{'This configuration defines all possible Screens to enable or disable dynamic fields.'} = 'Diese Konfiguration definiert alle möglichen Oberflächen um dynamische Felder zu aktivieren oder zu deaktivieren.';
 
     # DynamicFieldScreen
-    $Self->{Translation}->{'Dynamic Fields for Screen'} = 'Dynamische Felder für die Oberfläche';
-    $Self->{Translation}->{'Dynamic Fields Screens'}     = 'Dynamische Felder Oberflächen';
-    $Self->{Translation}->{'Assigned Required Fields'}  = 'Zugewiesene Pflichtfelder';
-    $Self->{Translation}->{'Dynamic Field Screen Management'}  = 'Dynamische Feldern Oberflächen Verwaltung';
+    $Self->{Translation}->{'Dynamic Fields for Screen'}       = 'Dynamische Felder für die Oberfläche';
+    $Self->{Translation}->{'Dynamic Fields Screens'}          = 'Dynamische Felder Oberflächen';
+    $Self->{Translation}->{'Assigned Required Fields'}        = 'Zugewiesene Pflichtfelder';
+    $Self->{Translation}->{'Dynamic Field Screen Management'} = 'Dynamische Feldern Oberflächen Verwaltung';
+    $Self->{Translation}->{'All views'}                       = 'Alle Oberflächen';
+
+    # Filter
+    $Self->{Translation}->{'Filter DynamicField Screen'}      = 'Oberflächen für Dynamische Felder filtern';
+    $Self->{Translation}->{'Filter available fields'}         = 'Verfügbare Felder filtern';
+    $Self->{Translation}->{'Filter assigned fields'}          = 'Zugewiesene Felder filtern';
+    $Self->{Translation}->{'Filter assigned required fields'} = 'Zugewiesene Pflichtfelder filtern';
+
 
     $Self->{Translation}->{'You can assign Fields to this Screen by dragging the elements with the mouse from the left list to the right list.'} = 'Sie können dieser Oberfläche Dynamische Felder zuweisen, indem Sie sie mit der Maus aus der rechten Liste in die linke Liste verschieben.';
     $Self->{Translation}->{'Manage dynamic field in screens.'} = 'Verwaltung von dynamischen Feldern in Oberflächen';
