5232b2a904e1b21d389cdf3d7dfc3dcd8fb5c51d
Denny Bresch <db@znuny.com>
2016-07-01 17:45:42 +0200

    Added js selection.

--- var/httpd/htdocs/js/Core.Agent.Admin.Znuny4OTRSDynamicFieldScreen.js
+++ var/httpd/htdocs/js/Core.Agent.Admin.Znuny4OTRSDynamicFieldScreen.js
@@ -28,29 +28,61 @@ Core.Agent.Admin.Znuny4OTRSDynamicFieldScreen = (function (TargetNS) {
 
             // if the element was removed from the AssignedElement list, rename it to DF name
             if (Target === 'AvailableElements') {
-                $(UI.item).find('input').attr('name', 'AvailableElements');
+                $(UI.item).find('input[type="hidden"]').attr('name', 'AvailableElements');
+                $(UI.item).find('input[type="checkbox"]').attr('name', 'SelectAllAvailableElements');
+                $(UI.item).find('input[type="checkbox"]').removeAttr('checked');
+
             }
 
             // rename it to AssignedElements
             else if (Target === 'AssignedElements') {
-                $(UI.item).find('input').attr('name', 'AssignedElements');
+                $(UI.item).find('input[type="hidden"]').attr('name', 'AssignedElements');
+                $(UI.item).find('input[type="checkbox"]').attr('name', 'SelectAllAssignedElements');
+                $(UI.item).find('input[type="checkbox"]').removeAttr('checked');
             }
 
             // rename it to AssignedRequiredElements
             else if (Target === 'AssignedRequiredElements') {
-                $(UI.item).find('input').attr('name', 'AssignedRequiredElements');
+                $(UI.item).find('input[type="hidden"]').attr('name', 'AssignedRequiredElements');
+                $(UI.item).find('input[type="checkbox"]').attr('name', 'SelectAllAssignedRequiredElements');
+                $(UI.item).find('input[type="checkbox"]').removeAttr('checked');
             }
         }
         // Initialize Allocation List
         Core.UI.AllocationList.Init("#AvailableElements, #AssignedElements, #AssignedRequiredElements", ".AllocationList", UpdateFields);
 
         // Initialize list filter
+        Core.UI.Table.InitTableFilter($('#FilterElements'), $('#Elements'));
         Core.UI.Table.InitTableFilter($('#FilterAvailableElements'), $('#AvailableElements'));
         Core.UI.Table.InitTableFilter($('#FilterAssignedElements'), $('#AssignedElements'));
         Core.UI.Table.InitTableFilter($('#FilterAssignedRequiredElements'), $('#AssignedRequiredElements'));
-        Core.UI.Table.InitTableFilter($('#FilterElements'), $('#Elements'));
 
-        // TODO:
+        $.each( ['SelectAllAvailableElements', 'SelectAllAssignedElements', 'SelectAllAssignedRequiredElements'], function (Index, Elements) {
+
+            console.log(Elements);
+            $('input[type="checkbox"][name="'+Elements+'"').bind('click', function () {
+                Core.Form.SelectAllCheckboxes($(this), $('#' + Elements));
+            });
+        });
+
+        // register all bindings
+        $.each( ['AvailableElements', 'AssignedElements', 'AssignedRequiredElements'], function (Index, ParameterName) {
+
+            var Element;
+            $('#AllSeleced'+ ParameterName).bind('click', function () {
+                // move all li to another ul list.
+                $("input:checkbox:checked").each(function(){
+                    Element = $(this).val();
+                    if(Element){
+                        $('li#'+Element).appendTo('#'+ ParameterName);
+                        $('li#'+Element).find('input[type="hidden"]').attr('name', ParameterName);
+                        $('#'+Element).find('input[type="checkbox"]').attr('name', 'SelectAll'+ ParameterName);
+                    }
+                });
+                $("input:checkbox").prop('checked', $(this).prop("checked")).removeAttr('checked');
+            });
+        });
+
         $('#Submit').bind('click', function() {
             $('#Form').submit();
             return false;
