1bf9e8afd6731efdb3353a8c3e68e3087ca5b9b8
Denny Bresch <db@znuny.com>
2017-10-02 11:15:34 +0200

    Refactoring of OutputfilterPost.

--- Kernel/Output/HTML/FilterElementPost/Znuny4OTRSAdvancedDynamicFields.pm
+++ Kernel/Output/HTML/FilterElementPost/Znuny4OTRSAdvancedDynamicFields.pm
@@ -34,98 +34,18 @@ sub Run {
     # check needed stuff
     return if !defined $Param{Data};
 
-    my $LanguageObject = $Kernel::OM->Get('Kernel::Language');
     my $LayoutObject   = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
 
-    my $ImportExportHTML = "<div class='WidgetSimple' id='DynamicFieldImportExportWidget'>
-            <div class='Header'>
-                <h2>" . $LanguageObject->Translate('Import / Export') . "</h2>
-            </div>
-            <div class='Content'>
-                <p class='FieldExplanation'>
-                    "
-        . $LanguageObject->Translate(
-        'Here you can upload a configuration file to import dynamic fields to your system. The file needs to be in .yml format as exported by dynamic field management module.'
-        )
-        . "
-                </p>
-                <ul class='ActionList SpacingTop'>
-                    <li>
-                        <form action='$LayoutObject->{CGIHandle}' method='post' enctype='multipart/form-data' class='Validate PreventMultipleSubmits'>
-                            <input type='hidden' name='ChallengeToken' value='$LayoutObject->{UserChallengeToken}'>
-                            "
-        . (
-        $LayoutObject->{SessionID}
-            && !$LayoutObject->{SessionIDCookie}
-        ? "<input type='hidden' name='$LayoutObject->{SessionName}' value='$LayoutObject->{SessionID}'>"
-        : ''
-        )
-        . "
-                            <input type='hidden' name='Action' value='AdminDynamicFieldImportExport'/>
-                            <input type='hidden' name='Subaction' value='Import'/>
-                            <input name='FileUpload' id='FileUpload' type='file' size='18' class='Fixed W100pc Validate_Required [% Data.FileUploadInvalid | html %]'/>
-                            <div id='FileUploadError' class='TooltipErrorMessage'><p>"
-        . $LanguageObject->Translate('This field is required.')
-        . "</p></div>
-                            <div id='FileUploadServerError' class='TooltipErrorMessage'><p>"
-        . $LanguageObject->Translate('This field is required.')
-        . "</p></div>
-                            <fieldset class='SpacingTop'>
-                                <input type='checkbox' id='OverwriteExistingEntitiesExample' name='OverwriteExistingEntities' value='1'/>
-                                <label for='OverwriteExistingEntitiesExample'>"
-        . $LanguageObject->Translate('Overwrite existing entities')
-        . "</label>
-                            </fieldset>
-                            <button class='CallForAction Fullsize Center SpacingTop' id='DynamicFieldImport' type='submit'>
-                                <span><i class='fa fa-upload'></i>"
-        . $LanguageObject->Translate('DynamicFields Import')
-        . "</span>
-                            </button>
-                        </form>
-                    </li>
-                    <li>
-                        <a href='$LayoutObject->{Baselink}Action=AdminDynamicFieldImportExport;Subaction=Export"
-        . (
-        $LayoutObject->{SessionID}
-            && !$LayoutObject->{SessionIDCookie} ? ";$LayoutObject->{SessionName}=$LayoutObject->{SessionID}" : ''
-        )
-        . "' id='DynamicFieldExport' class='CallForAction Fullsize Center'><span><i class='fa fa-download'></i>"
-        . $LanguageObject->Translate('DynamicFields Export')
-        . "</span></a>
-                    </li>
-                </ul>
-            </div>
-        </div>";
+    my $Output = $LayoutObject->Output(
+        TemplateFile => 'AdminDynamicFieldAdvanced',
+        Data         => {},
+    );
 
-    my $ScreensHTML = "<div class='WidgetSimple'>
-            <div class='Header'>
-                <h2>" . $LanguageObject->Translate('Dynamic Fields Screens') . "</h2>
-            </div>
-            <div class='Content'>
-                <p class='FieldExplanation'>
-                    "
-        . $LanguageObject->Translate('Here you can manage the dynamic fields in the respective screens.') . "
-                </p>
-                <ul class='ActionList SpacingTop'>
-                    <li>
-                        <a href='$LayoutObject->{Baselink}Action=AdminDynamicFieldScreen"
-        . (
-        $LayoutObject->{SessionID}
-            && !$LayoutObject->{SessionIDCookie} ? ";$LayoutObject->{SessionName}=$LayoutObject->{SessionID}" : ''
-        )
-        . "' class='CallForAction Fullsize Center'><span><i class='fa fa-plus-square'></i>"
-        . $LanguageObject->Translate('Dynamic Fields Screens')
-        . "</span></a>
-                    </li>
-                </ul>
-            </div>
-        </div>";
-
-    my $Label     = $LanguageObject->Translate('Hint');
-    my $RegExHTML = '(\<div class\=\"WidgetSimple\"\>\s+\<div class\=\"Header\"\>\s+\<h2\>' . $Label . '\<\/h2\>)';
-
-    # manipulate HTML content
-    ${ $Param{Data} } =~ s{$RegExHTML}{$ImportExportHTML$ScreensHTML$1}ms;
+    $LayoutObject->_OutputFilterHookInsertBefore(
+        Name    => 'Hint',
+        Content => $Output,
+        %Param,
+    );
 
     return 1;
 }
